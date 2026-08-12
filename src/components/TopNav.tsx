d184d8b091657d801cf617e60b249c3840478be1
somosmuzamarketing-spec <somosmuza.marketing@gmail.com>
2026-08-12 07:09:07 -0500

    Update TopNav.tsx

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -19,8 +19,7 @@ export default function TopNav({ name, avatarUrl, role, plan, isMentor }: TopNav
   return (
     <header className="navbar">
       <Link href="/dashboard" className="logo">
-        <span className="logo-mark">M</span>
-        <span className="logo-word">Muza</span>
+        <img src="/logo-horizontal.png" alt="Muza" className="logo-img" />
       </Link>
 
       <div className="navlinks">
