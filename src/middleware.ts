0823115bb22db2a6ac8dd12b73ce68a1705a4b99
somosmuzamarketing-spec <somosmuza.marketing@gmail.com>
2026-08-27 16:40:19 -0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,13 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/admin/:path*", "/rooms/:path*", "/video/:path*"],
+  matcher: [
+    "/dashboard/:path*",
+    "/admin/:path*",
+    "/rooms/:path*",
+    "/video/:path*",
+    "/celebremos/:path*",
+    "/colaboracion/:path*",
+    "/oportunidades/:path*",
+  ],
 };
